8616ebc765e611c1fe0e69d6d93512c77b65f136
Yeison Molina <yeison.molina91@gmail.com>
2024-07-17 13:53:47 -0500

    Clean theme

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,26 +1,22 @@
 import { useEffect, useMemo } from 'react';
 import { useFonts } from 'expo-font';
 import * as SplashScreen from 'expo-splash-screen';
-import { DefaultTheme, PaperProvider, MD3LightTheme as DarkTheme } from 'react-native-paper'
+import { DefaultTheme, PaperProvider } from 'react-native-paper'
 import 'react-native-reanimated';
 
 import { StoreProvider } from '@/domain/store/StoreProvider';
 import AppRoutes from '@/app/_routes';
-import { useColorScheme } from 'react-native';
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  const colorSchema = useColorScheme();
 
   const [loaded] = useFonts({
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
   });
 
-  console.log(colorSchema)
   const theme = useMemo(() => ({
-    ...(colorSchema === 'dark' ? DarkTheme : DefaultTheme),
     colors: {
       ...DefaultTheme.colors,
       primary: '#003670',
